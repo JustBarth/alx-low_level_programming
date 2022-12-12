5016d75f2e6748f84b13e34b9b544f88b83bfcb3
Foralex Barth <foralex.barth@gmail.com>
2022-12-12 14:04:43 -0800

    Em

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit < 8; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -23,7 +23,7 @@ int main(void)
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 				putchar(',');
-				putchar(' ')'
+				putchar(' ');
 			}
 		}
 	}
