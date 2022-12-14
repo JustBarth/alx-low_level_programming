7ac1952ad07f5a6199d2f4c7cf24e90932f93159
Foralex Barth <foralex.barth@gmail.com>
2022-12-13 19:29:35 -0800

    First commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ *
+ * main - print alphabet in lowercase, then new line
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	-putchar('\n');
+}
